0f736d4b677d660c48f69918fc1439d358016daf
hslnoni-tho <9892@holbertonstudents.com>
2025-01-13 14:43:14 +0100

    fix the code of print_argument.py again

--- print_arguments.py
+++ print_arguments.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-for i in range(1, len(sys.argv)):  # Commencer à partir de l'index 1 pour exclure le nom du fichier
+for i in range(1, len(sys.argv)):  # Je commence à partir de l'index 1 pour exclure le nom du fichier
     print(sys.argv[i])
