6093d679b94837ed604cb9e535aec5e65ab7fd2f
hslnoni-tho <9892@holbertonstudents.com>
2025-01-13 14:47:26 +0100

    fix the code of factorial.py again

--- factorial.py
+++ factorial.py
@@ -5,9 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  
     return result
 
-f = factorial(int(sys.argv[1]))
+f = factorial(int(sys.argv[1]))  # je prend ici l'argument de la ligne de commande
 print(f)
-
