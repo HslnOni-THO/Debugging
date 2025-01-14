75d70fac1ad0428b894d6590c9c89287386d18b3
hslnoni-tho <9892@holbertonstudents.com>
2025-01-14 13:52:27 +0100

    fix my code for print_arguments

--- print_arguments.py
+++ print_arguments.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-for i in range(1, len(sys.argv)):  # Je commence à partir de l'index 1 pour exclure le nom du fichier
+for i in range(1, len(sys.argv)):  # Start from index 1 to skip the script name
     print(sys.argv[i])
