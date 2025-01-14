3946d08ed3a0e7c484b0a41fd6388c63ffd9f50b
hslnoni-tho <9892@holbertonstudents.com>
2025-01-14 13:49:20 +0100

    fix my code for factorial_recursive

--- factorial_recursive.py
+++ factorial_recursive.py
@@ -2,26 +2,10 @@
 import sys
 
 def factorial(n):
-    """
-    Calculate the factorial of a given number using recursion.
-
-    Parameters:
-    n (int): The number for which the factorial is to be calculated.
-
-    Returns:
-    int: The factorial of the given number.
-    """
-    if n == 0:
+    if n == 0:  # Properly indented
         return 1
     else:
         return n * factorial(n - 1)
 
-# Check if an argument is provided
-if len(sys.argv) < 2:
-    print("Usage: ./factorial_recursive.py <number>")
-else:
-    # Read input from the command line argument
-    f = factorial(int(sys.argv[1]))
-
-    # Output the result
-    print(f)    
+f = factorial(int(sys.argv[1]))
+print(f)
